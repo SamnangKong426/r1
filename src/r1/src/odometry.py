40a98772e1049f94d1dfedf32c3ad8a71cb8ed15
SamnangKong426 <samnangkong499@gmail.com>
2024-07-02 12:06:13 +0700

    update

--- src/r1/src/odometry.py
+++ src/r1/src/odometry.py
@@ -34,6 +34,7 @@ class OdometryNode(Node):
         self.x = 500.0
         self.y = 0.0
         self.w = 0.0
+        print("Odometry Node is running")
 
     def listener_callback(self, msg: Odometry):
         self.get_logger().info('I heard: "%s"' % str(msg))
@@ -49,6 +50,7 @@ class OdometryNode(Node):
         self.x = msg.x  # next position x
         self.y = msg.y  # next position y
         self.w = msg.z  # next position w
+        print("listen: {}, {}, {}".format(self.x, self.y, self.w))
 
     def quaternion_to_rpy(self, rs_x, rs_y, rs_z, rs_w):
         w = rs_w
