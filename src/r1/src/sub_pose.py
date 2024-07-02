25f05d1c2ff338a108a3a763095e9b9452f73974
SamnangKong426 <samnangkong499@gmail.com>
2024-07-02 12:36:23 +0700

    update

--- src/r1/src/sub_pose.py
+++ src/r1/src/sub_pose.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Odometry
 
 class CameraPoseSubscriber(Node):
     def __init__(self):
         super().__init__('camera_pose_subscriber')
         self.subscription = self.create_subscription(
-            Twist,
+            Odometry,
             '/camera/pose/sample',
             self.listener_callback,
             10)
