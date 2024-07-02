3d727c00671559a48d9a529473a1c0aa0e4ca9d7
SamnangKong426 <samnangkong499@gmail.com>
2024-07-02 19:37:28 +0700

    update

--- src/r1/src/odometry.py
+++ src/r1/src/odometry.py
@@ -85,7 +85,7 @@ class OdometryNode(Node):
         self.Ix = (self.Ix + dx) * 0.2
         Py = dy * 0.5
         self.Iy = (self.Iy + dy) * 0.2
-        Pw = dw * 0.005
+        Pw = dw * 0.01
         self.Iw = (self.Iw + dw) * 0.01
         vx = Px+ self.Ix
         vy = Py+ self.Iy
