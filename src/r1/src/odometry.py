08a338ea9f05946c65425189b2d3bae62adfbc0b
SamnangKong426 <samnangkong499@gmail.com>
2024-07-04 13:50:32 +0700

    update

--- src/r1/src/odometry.py
+++ src/r1/src/odometry.py
@@ -107,11 +107,11 @@ class OdometryNode(Node):
             # return 0.0, 0.0, 0.0
         # Calculate velocities based on distances to target
 
-        Px = dx * 0.1                   # 0.5
-        self.Ix = (self.Ix + dx) * 0 # 0.2
-        Py = dy * 0.1                   # 0.5
-        self.Iy = (self.Iy + dy) * 0  # 0.2
-        Pw = dw * 0.04                # 0.1
+        Px = dx * 0.5                   # 0.5
+        self.Ix = (self.Ix + dx) * 0.2 # 0.2
+        Py = dy * 0.5                   # 0.5
+        self.Iy = (self.Iy + dy) * 0.2  # 0.2
+        Pw = dw * 0.1                # 0.1
         self.Iw = (self.Iw + dw) * 0  # 0.1
         vx = Px + self.Ix
         vy = Py + self.Iy
